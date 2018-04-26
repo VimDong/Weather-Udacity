7b53dd9d3e61ebeea78e220ca7079c5ea9514cde
Vim <dongchongle@gmail.com>
2018-04-26 18:45:19 +0800

    3-2 通过 API 获取数据，提取气温和天气

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,20 @@
 //index.js
 Page({
   onLoad() {
-    console.log("Hello World!")
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '深圳市'
+      },
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function(res){
+        console.log(res.data);
+        let result = res.data.result;
+        let temp = result.now.temp
+        let weather = result.now.weather
+        console.log(temp, weather);
+      }
+    })
   }
 })
