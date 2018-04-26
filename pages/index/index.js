ef688531da7c76070efe0bca9b3ade071a0be1b7
Vim <dongchongle@gmail.com>
2018-04-26 19:32:46 +0800

    3-3 更新气温和天气数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,20 @@
 //index.js
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
 Page({
+  data: {
+    nowTemp: '14',
+    nowWeather: '阴天'
+  },
   onLoad() {
+    var that = this
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now',
       data: {
@@ -8,12 +22,17 @@ Page({
       },
       method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
       // header: {}, // 设置请求的 header
-      success: function(res){
+      success: res => {
         console.log(res.data);
         let result = res.data.result;
         let temp = result.now.temp
         let weather = result.now.weather
         console.log(temp, weather);
+
+        this.setData({
+          nowTemp: temp + '°C',
+          nowWeather: weatherMap[weather]
+        })
       }
     })
   }
